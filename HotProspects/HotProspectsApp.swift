cddac49fb5e96cc74f36a311e3beb0e82cd0ffbd
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-15 11:18:15 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kenji Dela Cruz on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
